4425d5b368417d96ec742c2cc84598d44ae80a89
ShaharAshe <shaharas30@gmail.com>
2024-04-17 14:56:55 +0300

    //

--- src/ex2/Main.java
+++ src/ex2/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         try{
             /* You can also read urls from the terminal */
-            // Controller urlCrawler = new Controller(args, "image/", new TerminalReaderComp());
+            // Controller urlCrawler = new Controller(args, new TerminalReaderComp());
 
             Controller urlCrawler = new Controller(args, new FileReaderComp(args[FILENAME]));
             urlCrawler.crawl();
